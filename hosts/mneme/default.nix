58f0ee8a260ade9a856643b2a738bfc2e3465bb0
Scott Burns <scott@lentigo.net>
2025-05-22 10:17:11 -0400

    feat(mneme-host): install nix-ld
    Using this to try to fix

--- hosts/mneme/default.nix
+++ hosts/mneme/default.nix
@@ -54,4 +54,8 @@
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = [
+    pkgs.pcsclite
+  ];
 }
