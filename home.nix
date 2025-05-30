e7fedccf07b419c2504bc61ef984f54a1e798545
Scott Burns <scott@lentigo.net>
2025-05-30 12:56:08 -0400

    feat(tmux): start TMUX at login on GraySlab

--- home.nix
+++ home.nix
@@ -123,4 +123,9 @@
 
   # enable XDG directory support
   xdg.enable = true;
+
+  # override TMUX AUTOSTART for Grayslab
+  programs.zsh.oh-my-zsh.extraConfig = lib.mkForce ''
+    ZSH_TMUX_AUTOSTART=true
+  '';
 }
