7c7d6c8718fa54a2ee65b5c86aba9ea73a822114
Scott Burns <scott@lentigo.net>
2025-06-08 12:32:05 -0400

    feat(nixos): set trusted-users to root, scott

--- hosts/common.nix
+++ hosts/common.nix
@@ -57,4 +57,7 @@
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Set trusted users
+  nix.settings.trusted-users = [ "root" "scott" ];
 }
