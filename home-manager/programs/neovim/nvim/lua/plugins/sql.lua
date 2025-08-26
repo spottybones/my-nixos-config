222bb3d6370c6cd84661fe8c3e5bf8c11fd2eb51
Scott Burns <scott@lentigo.net>
2025-08-26 15:34:37 -0400

    fix(neovim): specify linting args for sqlfluff
    nvim-lint specifies ansi sql dialect as default on the command line
    which causes dialect specification via config files to be ignored. Fix
    this by specifying args that don't include the dialect.

--- home-manager/programs/neovim/nvim/lua/plugins/sql.lua
+++ home-manager/programs/neovim/nvim/lua/plugins/sql.lua
@@ -16,6 +16,9 @@ return {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = function(_, opts)
+      opts.linters["sqlfluff"] = {
+        args = { "lint", "--format=json" },
+      }
       for _, ft in ipairs(sql_ft) do
         opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
         table.insert(opts.linters_by_ft[ft], "sqlfluff")
